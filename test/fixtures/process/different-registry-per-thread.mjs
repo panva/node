2cc4c81e10b89297fe8442f312bcacd46a87cceb
Antoine du Hamel <duhamelantoine1995@gmail.com>
2026-05-11 14:10:43 +0200

    Revert "test: reduce flakiness of `different-registry-per-thread`"
    This reverts commit 130398ab0554f8ba4cb1eea3ac9098d844335b2d.
    
    PR-URL: https://github.com/nodejs/node/pull/63244
    Reviewed-By: Richard Lau <richard.lau@ibm.com>
    Reviewed-By: Vinícius Lourenço Claro Cardoso <contact@viniciusl.com.br>
    Reviewed-By: Stefan Stojanovic <stefan.stojanovic@janeasystems.com>

--- test/fixtures/process/different-registry-per-thread.mjs
+++ test/fixtures/process/different-registry-per-thread.mjs
@@ -1,26 +1,15 @@
 import { isMainThread, Worker } from 'node:worker_threads';
 
-const registeredRefs = [];
-
-const ref = { foo: 'foo' };
-registeredRefs.push(ref);
-
 if (isMainThread) {
-  process.finalization.register(ref, () => {
+  process.finalization.register({ foo: 'foo' }, () => {
     process.stdout.write('shutdown on main thread\n');
   });
 
   const worker = new Worker(import.meta.filename);
 
-  worker.on('error', (err) => {
-    // Referencing `registeredRefs` here to avoid `ref` being GCed before the worker exits.
-    console.log(registeredRefs);
-    throw err;
-  });
-
   worker.postMessage('ping');
 } else {
-  process.finalization.register(ref, () => {
+  process.finalization.register({ foo: 'bar' }, () => {
     process.stdout.write('shutdown on worker\n');
   });
 }
